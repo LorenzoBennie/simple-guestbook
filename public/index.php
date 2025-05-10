afa237042b645bed5c29b613c480d694f4db6825
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2025-05-10 12:02:28 +0200

    render simple views

--- public/index.php
+++ public/index.php
@@ -7,8 +7,10 @@ session_start();
 // Create a constant reference to the includes folder for convenience
 const INCLUDES_DIR = __DIR__ . '/../includes';
 const ROUTES_DIR = __DIR__ . '/../routes';
+const TEMPLATES_DIR = __DIR__ . '/../templates';
 
 require_once INCLUDES_DIR . '/router.php';
+require_once INCLUDES_DIR . '/view.php';
 
 // Handle request
 dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
---
+++ templates/index_get.php
@@ -0,0 +1,7 @@
+<main>
+
+<section>
+     <h2>Home</h2>
+     <p>Welcome to the Home Page!</p>
+</section>
+</main>
