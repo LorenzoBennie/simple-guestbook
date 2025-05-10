0b5cec02c4c5a7f1fadeaefad2bbd85da76d6238
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2025-05-10 13:17:19 +0200

    create CLI script to run schema file

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,24 @@
 <?php
 
 //connect to the database
+
+use PgSql\Lob;
+
 function connectDb(): PDO {
     $pdo = new PDO('sqlite:' . DB_DIR . '/db.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     return $pdo;
+}
+
+// used to run the schema in  the CLI
+function loadSchema(PDO $pdo, string $schemaFile): void {
+    $sql = file_get_contents($schemaFile);
+    if (false === $sql) {
+        die("Failed to load schema from file: $schemaFile");
+    }
+
+    $pdo->exec($sql);
+    echo "Schema successfully.\n";
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -2,17 +2,10 @@
 //This file is the entry point to the application
 
 declare(strict_types=1);
+
+require_once '../bootstrap.php';
+
 session_start();
-
-// Create a constant reference to the includes folder for convenience
-const INCLUDES_DIR = __DIR__ . '/../includes';
-const ROUTES_DIR = __DIR__ . '/../routes';
-const TEMPLATES_DIR = __DIR__ . '/../templates';
-const DB_DIR = __DIR__ . '/../db';
-
-require_once INCLUDES_DIR . '/router.php';
-require_once INCLUDES_DIR . '/view.php';
-require_once INCLUDES_DIR . '/db.php';
 
 // Handle request
 dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
